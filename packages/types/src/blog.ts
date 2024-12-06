be78a98a1c17d715b49d429e510966c4717f1887
Lokendra <anizen242@gmail.com>
2024-12-06 12:13:42 +0530

    Explicit imports in index.ts to fix types not being found

--- packages/types/src/blog.ts
+++ packages/types/src/blog.ts
@@ -1,4 +1,4 @@
-export {}
+
 
 interface TiptapDocument {
   type: "doc";
--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,3 +1,44 @@
-export * from "./blog";
-export * from "./blogSchema"
-export * from "./user"
+import {
+  TiptapDocument,
+  ParagraphNode,
+  HardBreakNode,
+  HeadingNode,
+  BulletListNode,
+  ListItemNode,
+  CodeBlockNode,
+  BlockquoteNode,
+  TextContent,
+  Mark,
+  ImageNode,
+  OrderedListNode,
+} from "./blog";
+
+import {
+  OutlineSectionSchema,
+  BlogOutlineSchema,
+  OutlineSectionType,
+  BlogOutlineType,
+} from "./blogSchema";
+
+import { UserSchema, UserSchemaType } from "./user";
+
+export {
+    TiptapDocument,
+    ParagraphNode,
+    HardBreakNode,
+    HeadingNode,
+    BulletListNode,
+    ListItemNode,
+    CodeBlockNode,
+    BlockquoteNode,
+    TextContent,
+    Mark,
+    ImageNode,
+    OrderedListNode,
+    OutlineSectionSchema,
+    OutlineSectionType,
+    BlogOutlineSchema,
+    BlogOutlineType,
+    UserSchema,
+    UserSchemaType
+}
